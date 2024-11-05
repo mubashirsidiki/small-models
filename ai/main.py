cf945fd77436068f2224b6124bd0eae682f7221b
Mubbashir Ali Minhas <mubbashir917@gmail.com>
2024-11-05 21:35:21 +0500

    errand: added cors with all configuration

--- ai/main.py
+++ ai/main.py
@@ -3,8 +3,21 @@
 import uvicorn
 from fastapi import FastAPI
 from app.routers import flant5_router, yolo_face_router
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+middlewares = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    ),
+]
+
+app = FastAPI(
+   middleware=middlewares
+)
 
 # Include routers
 app.include_router(flant5_router.router, prefix="/small-model/flant5", tags=["FLAN-T5"])
